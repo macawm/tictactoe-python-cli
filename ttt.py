11a3e85ab08662a689208f246a5c41db198df125
Anthony Magee <macawm@gmail.com>
2021-06-15 21:41:04 -0600

    Make basic game play and end state

--- ttt.py
+++ ttt.py
@@ -1,10 +1,33 @@
 # Tic-Tac-Toe
 
+winner = -1
+board = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+turn = 0
+maxMoves = 9
+
 def printIntro():
 	print("Tic-Tac-Toe Game")
 
+def startGame():
+	currentMove = 0
+	while winner == -1 and currentMove < maxMoves:
+		takeTurn()
+		checkForWin()
+		printBoard()
+		currentMove += 1
+
+def takeTurn():
+	pass
+
+def checkForWin():
+	pass
+
+def printBoard():
+	print(board)
+
 def main():
 	printIntro()
+	startGame()
 
 if __name__ == '__main__':
 	main()
