0d8168147ec6cde491b1dc082f0eb6622ea07d16
Anthony Magee <macawm@gmail.com>
2021-06-17 10:45:40 -0600

    move the procedural code to it's place in the classes

--- ttt.py
+++ ttt.py
@@ -1,133 +1,5 @@
 # Tic-Tac-Toe
 
-XX = "X"
-OO = "O"
-
-GS_NOT_DONE = -1
-GS_CAT = 0
-GS_XWIN = 1
-GS_OWIN = 2
-
-def printIntro():
-	print("Tic-Tac-Toe Game")
-	print("q to quit\n")
-
-def signOff():
-	print("Thanks for playing.")
-
-def startGame() -> None:
-	global XX,OO
-	global GS_CAT,GS_OWIN,GS_XWIN,GS_NOT_DONE
-
-	maxMoves = 9
-	gameState = GS_NOT_DONE
-
-	helpBoard = [1,2,3,4,5,6,7,8,9]
-	board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-
-	currentMove = 0
-	printBoard(helpBoard, GS_NOT_DONE)
-
-	while gameState == GS_NOT_DONE:
-		takeTurn(board, currentMove)
-
-		if (isXsTurn(currentMove)):
-			gameState = GS_XWIN if checkForWin(board, XX) else GS_NOT_DONE
-		else:
-			gameState = GS_OWIN if checkForWin(board, OO) else GS_NOT_DONE
-
-		currentMove += 1
-
-		if (currentMove == maxMoves and not (gameState == GS_XWIN or gameState == GS_OWIN)):
-			gameState = GS_CAT
-
-		printBoard(board, gameState)
-
-	signOff()
-
-def takeTurn(board: list, move: int) -> int:
-	newMark = getUserInput(board, move)
-	updateBoard(board, newMark, move)
-	return newMark
-
-def getUserInput(board: list, move: int) -> int:
-	badInput = True
-	while (badInput):
-		square = getPlayerInput(move)
-		
-		if (square.isdigit()):
-			sInt = int(square)
-			if (not squareInRange(sInt) or not squareIsOpen(board, sInt)):
-				square = handleBadInput()
-			else:
-				badInput = False
-		elif (square == 'q'):
-			quit()
-		else:
-			square = handleBadInput()
-
-	return sInt
-
-def getPlayerInput(move: int) -> str:
-	player = XX
-	if (not isXsTurn(move)):
-		player = OO
-
-	return input("%s make your move. " % player)
-
-def handleBadInput() -> bool:
-	print("Bad selection. Try again.")
-	return True
-
-def updateBoard(board: list, s: int, move: int):
-	global XX,OO
-	board[s-1] = XX if isXsTurn(move) else OO
-
-def isXsTurn(currentMove: int) -> bool:
-	return currentMove % 2 == 0
-
-def squareInRange(s: int) -> bool:
-	return s >= 1 and s <= 9
-
-def squareIsOpen(board: list, s: int) -> bool:
-	global XX,OO
-	return board[s-1] not in (XX,OO)
-
-def checkForWin(board: list, player: str) -> bool:
-	win = False
-
-	for bi in range(3):
-		win = win or (board[bi*3] == player and board[bi*3+1] == player and board[bi*3+2] == player)
-		win = win or (board[bi] == player and board[bi+3] == player and board[bi+6] == player)
-
-	bi = 0
-	#diag l
-	win = win or (board[bi] == player and board[bi+4] == player and board[bi+8] == player)
-	
-	bi = 8
-	#diag r
-	win = win or (board[bi] == player and board[bi-4] == player and board[bi-8] == player)
-
-	return win
-
-def printBoard(board: list, gameState: int):
-	global GS_CAT,GS_OWIN,GS_XWIN
-
-	bi = 0
-	print("   %s|%s|%s" %(board[bi],board[bi+1],board[bi+2]))
-	print("   -----")
-	bi += 3
-	print("   %s|%s|%s" %(board[bi],board[bi+1],board[bi+2]))
-	print("   -----")
-	bi += 3
-	print("   %s|%s|%s" %(board[bi],board[bi+1],board[bi+2]))
-	print()
-
-	if (gameState == GS_XWIN or gameState == GS_OWIN):
-		print("%s Win!!!" % ("Xs" if (gameState == GS_XWIN) else "Os"))
-	elif (gameState == GS_CAT):
-		print("Cat's game")
-
 def main():
 	game = Game()
 	game.start()
@@ -145,28 +17,60 @@ class GameState:
 
 class GameBoard:
 	def __init__(self, size: int):
-		self.size = size
-		self.__helpInfo = [i for i in range(1, size**2 + 1)]
-		self.__data = [' ' for _ in range(size**2)]
+		self.size = 3
+		self.__helpInfo = [i for i in range(1, self.size**2 + 1)]
+		self.__data = [' ' for _ in range(self.size**2)]
 
 	def print_help(self):
-		self.print_board(self.__helpInfo, GameState.NOT_DONE)
+		self.__print_board(self.__helpInfo, GameState.NOT_DONE)
 
-	def print_board(self, board: list, state: GameState):
+	def print_board(self, state: GameState):
+		self.__print_board(self.__data, state)
+
+	def __print_board(self, board: list, state: GameState):
 		bi = 0
-		print("   %s|%s|%s" % (board[bi], board[bi+1], board[bi+2]))
+		print("   %s|%s|%s" % tuple([board[i] for i in range(bi, bi + self.size)]))
 		print("   -----")
 		bi += 3
-		print("   %s|%s|%s" % (board[bi], board[bi+1], board[bi+2]))
+		print("   %s|%s|%s" % tuple([board[i] for i in range(bi, bi + self.size)]))
 		print("   -----")
 		bi += 3
-		print("   %s|%s|%s" % (board[bi], board[bi+1], board[bi+2]))
+		print("   %s|%s|%s" % tuple([board[i] for i in range(bi, bi + self.size)]))
 		print()
 
 		if state == GameState.X_WIN or state == GameState.O_WIN:
 			print("%s Win!!!" % ("Xs" if (state == GameState.X_WIN) else "Os"))
 		elif state == GameState.CAT:
 			print("Cat's game")
+
+	def square_in_range(self, s_int):
+		return s_int in range(1, self.size**2 + 1)
+
+	def square_is_open(self, s_int):
+		return self.__data[s_int - 1] not in ('X', 'O')
+
+	def allowed_moves(self):
+		return self.size**2
+
+	def check_for_win(self, player: str) -> bool:
+		win = False
+
+		for bi in range(self.size):
+			win = win or (self.__data[bi*3] == player and self.__data[bi*3+1] == player and self.__data[bi*3+2] == player)
+			win = win or (self.__data[bi] == player and self.__data[bi+3] == player and self.__data[bi+6] == player)
+
+		bi = 0
+		#diag l
+		win = win or (self.__data[bi] == player and self.__data[bi+4] == player and self.__data[bi+8] == player)
+
+		bi = 8
+		#diag r
+		win = win or (self.__data[bi] == player and self.__data[bi-4] == player and self.__data[bi-8] == player)
+
+		return win
+
+	def update(self, new_mark: int, x_turn: bool) -> None:
+		self.__data[new_mark-1] = 'X' if x_turn else 'O'
 
 
 class GamePlayer:
@@ -177,6 +81,7 @@ class GamePlayer:
 
 class Game:
 	__state = None
+	__move = None
 
 	def __init__(self):
 		self.__board = GameBoard(3)
@@ -187,10 +92,76 @@ class Game:
 		self.print_intro()
 		self.__state = GameState.NOT_DONE
 
+		self.__move = 0
+
+		while self.game_not_over():
+			self.take_turn()
+
+			if self.is_x_turn():
+				self.__state = GameState.X_WIN if self.__board.check_for_win('X') else GameState.NOT_DONE
+			else:
+				self.__state = GameState.O_WIN if self.__board.check_for_win('O') else GameState.NOT_DONE
+
+			self.__move += 1
+
+			if self.reached_max_moves() and not (self.xWon() or self.oWon()):
+				self.__state = GameState.CAT
+
+			self.__board.print_board(self.__state)
+
 	def print_intro(self):
 		print("Tic-Tac-Toe Game (%s x %s)" % (self.__board.size, self.__board.size))
 		print("q to quit\n")
 		self.__board.print_help()
+
+	def game_not_over(self):
+		return self.__state == GameState.NOT_DONE
+
+	def take_turn(self) -> int:
+		new_mark = self.get_user_input()
+		self.__board.update(new_mark, self.is_x_turn())
+		return new_mark
+
+	def get_user_input(self) -> int:
+		bad_input = True
+		while bad_input:
+			square = self.get_raw_input()
+
+			if square.isdigit():
+				s_int = int(square)
+				if not self.__board.square_in_range(s_int) or not self.__board.square_is_open(s_int):
+					bad_input = self.handle_bad_input()
+				else:
+					bad_input = False
+			elif square == 'q':
+				quit()
+			else:
+				bad_input = self.handle_bad_input()
+
+		return s_int
+
+	def get_raw_input(self) -> str:
+		player = 'X'
+		if not self.is_x_turn():
+			player = 'O'
+
+		return input("%s make your move. " % player)
+
+	def is_x_turn(self) -> bool:
+		return self.__move % 2 == 0
+
+	def handle_bad_input(self) -> None:
+		print("Bad selection. Try again.")
+		return True
+
+	def reached_max_moves(self) -> bool:
+		return self.__move == self.__board.allowed_moves()
+
+	def xWon(self) -> bool:
+		return self.__state == GameState.X_WIN
+
+	def oWon(self) -> bool:
+		return self.__state == GameState.O_WIN
 
 
 if __name__ == '__main__':
