2cb820da459a47f573da3e3cc75b056d677b68f3
Anthony Magee <macawm@gmail.com>
2021-06-17 17:28:10 -0600

    move GamePlayer class up move printing of winner info out of board class to game runner refactor win logic to just tell us the winner instead of having extra states move 'move' updating to turn taking logic

--- src/ttt.py
+++ src/ttt.py
@@ -10,23 +10,31 @@ def main():
 class GameState(IntEnum):
 	NOT_DONE = -1
 	CAT = 0
-	X_WIN = 1
-	O_WIN = 2
+	GAME_WON = 1
+
+
+class GamePlayer:
+	def __init__(self, name: str, marker: str):
+		self.name = name
+		self.marker = marker
 
 
 class GameBoard:
-	def __init__(self, size: int):
+	def __init__(self, size: int, p1: GamePlayer, p2: GamePlayer):
 		self.size = size  # TODO handle different size boards
 		self.__helpInfo = [i for i in range(1, self.size**2 + 1)]
 		self.__data = [' ' for _ in range(self.size**2)]
 
+		self.__player1 = p1
+		self.__player2 = p2
+
 	def print_help(self) -> None:
-		self.__print_board(self.__helpInfo, GameState.NOT_DONE)
+		self.__print_board(self.__helpInfo)
 
-	def print_board(self, state: GameState) -> None:
-		self.__print_board(self.__data, state)
+	def print_board(self) -> None:
+		self.__print_board(self.__data)
 
-	def __print_board(self, board: list, state: GameState) -> None:
+	def __print_board(self, board: list) -> None:
 		bi = 0
 		print("   %s|%s|%s" % tuple([board[i] for i in range(bi, bi + self.size)]))
 		print("   -----")
@@ -37,44 +45,41 @@ class GameBoard:
 		print("   %s|%s|%s" % tuple([board[i] for i in range(bi, bi + self.size)]))
 		print()
 
-		if state == GameState.X_WIN or state == GameState.O_WIN:
-			print("%s Win!!!" % ("Xs" if (state == GameState.X_WIN) else "Os"))
-		elif state == GameState.CAT:
-			print("Cat's game")
-
 	def square_in_range(self, s_int: int) -> bool:
 		return s_int in range(1, self.size**2 + 1)
 
 	def square_is_open(self, s_int: int) -> bool:
 		if self.square_in_range(s_int):
-			return self.mark_at_square(s_int) not in ('X', 'O')
+			return self.mark_at_square(s_int) == ' '
 		else:
 			return False
 
 	def allowed_moves(self) -> int:
 		return self.size**2
 
-	def check_for_win(self, player: str) -> bool:
-		win = False
+	def get_winner(self, player: GamePlayer) -> GamePlayer:
+		return player if self.__did_win(player.marker) else None
 
+	def __did_win(self, mark: str) -> bool:
+		win = False
 		for bi in range(self.size):
-			win = win or (self.__data[bi*3] == player and self.__data[bi*3+1] == player and self.__data[bi*3+2] == player)
-			win = win or (self.__data[bi] == player and self.__data[bi+3] == player and self.__data[bi+6] == player)
+			win = win or (self.__data[bi*self.size] == mark and self.__data[bi*self.size+1] == mark and self.__data[bi*self.size+2] == mark)
+			win = win or (self.__data[bi] == mark and self.__data[bi+self.size] == mark and self.__data[bi+self.size*2] == mark)
 
 		bi = 0
 		# diag l
-		win = win or (self.__data[bi] == player and self.__data[bi+4] == player and self.__data[bi+8] == player)
+		win = win or (self.__data[bi] == mark and self.__data[bi+4] == mark and self.__data[bi+8] == mark)
 
 		bi = 2
 		# diag r
-		win = win or (self.__data[bi] == player and self.__data[bi+2] == player and self.__data[bi+4] == player)
+		win = win or (self.__data[bi] == mark and self.__data[bi+2] == mark and self.__data[bi+4] == mark)
 
 		return win
 
-	def update(self, square: int, x_turn: bool) -> None:
+	def update(self, square: int, player: GamePlayer) -> None:
 		""" This is the first, of only two, places where we alter the user input to correspond with our data store. """
 		if self.square_in_range(square):
-			self.__data[square - 1] = 'X' if x_turn else 'O'
+			self.__data[square - 1] = player.marker
 
 	def mark_at_square(self, square: int) -> str:
 		""" This is the second, of only two, places where we alter the user input to correspond with our data store. """
@@ -82,54 +87,59 @@ class GameBoard:
 			return self.__data[square-1]
 
 
-class GamePlayer:
-	def __init__(self, name: str, marker: str):
-		self.__name = name
-		self.__marker = marker
-
-
 class Game:
 	__state = None
 	__move = None
 
 	def __init__(self):
-		self.__board = GameBoard(3)
+		self.__winner = None
 		self.__xPlayer = GamePlayer('Player 1', 'X')
-		self.__yPlayer = GamePlayer('Player 2', 'O')
+		self.__oPlayer = GamePlayer('Player 2', 'O')
+		self.__firstPlayer = self.__xPlayer
+		self.__board = GameBoard(3, self.__xPlayer, self.__oPlayer)
 
 	def start(self):
-		self.print_intro()
+		self.__print_intro()
 		self.__state = GameState.NOT_DONE
 
 		self.__move = 0
 
 		while self.game_not_over():
 			self.take_turn()
+			self.find_winner()
 
-			if self.is_x_turn():
-				self.__state = GameState.X_WIN if self.__board.check_for_win('X') else GameState.NOT_DONE
-			else:
-				self.__state = GameState.O_WIN if self.__board.check_for_win('O') else GameState.NOT_DONE
-
-			self.__move += 1
-
-			if self.reached_max_moves() and not (self.x_won() or self.o_won()):
+			if self.reached_max_moves() and not self.has_winner():
 				self.__state = GameState.CAT
 
-			self.__board.print_board(self.__state)
+			self.__board.print_board()
+			self.__print_winner()
 
-	def print_intro(self):
+	def __print_winner(self) -> None:
+		if self.__winner is not None:
+			print("%s Wins!!!" % self.__winner.name)
+		elif self.__state == GameState.CAT:
+			print("Cat's game")
+
+	def find_winner(self) -> None:
+		self.__winner = self.__board.get_winner(self.get_current_player())
+		if self.has_winner():
+			self.__state = GameState.GAME_WON
+
+	def has_winner(self) -> bool:
+		return self.__winner is not None
+
+	def __print_intro(self) -> None:
 		print("Tic-Tac-Toe Game (%s x %s)" % (self.__board.size, self.__board.size))
 		print("q to quit\n")
 		self.__board.print_help()
 
-	def game_not_over(self):
+	def game_not_over(self) -> bool:
 		return self.__state == GameState.NOT_DONE
 
-	def take_turn(self) -> int:
+	def take_turn(self) -> None:
 		new_mark = self.get_user_input()
-		self.__board.update(new_mark, self.is_x_turn())
-		return new_mark
+		self.__board.update(new_mark, self.get_current_player())
+		self.__move += 1
 
 	def get_user_input(self) -> int:
 		bad_input = True
@@ -152,23 +162,16 @@ class Game:
 		return s_int
 
 	def get_raw_input(self) -> str:
-		player = 'X'
-		if not self.is_x_turn():
-			player = 'O'
+		return input("%s make your move. " % self.get_current_player().name)
 
-		return input("%s make your move. " % player)
-
-	def is_x_turn(self) -> bool:
-		return self.__move % 2 == 0
+	def get_current_player(self) -> GamePlayer:
+		return self.__xPlayer if self.__move % 2 == 0 else self.__oPlayer
 
 	def reached_max_moves(self) -> bool:
 		return self.__move == self.__board.allowed_moves()
 
-	def x_won(self) -> bool:
-		return self.__state == GameState.X_WIN
-
-	def o_won(self) -> bool:
-		return self.__state == GameState.O_WIN
+	def game_won(self) -> bool:
+		return self.__winner is not None
 
 
 if __name__ == '__main__':
